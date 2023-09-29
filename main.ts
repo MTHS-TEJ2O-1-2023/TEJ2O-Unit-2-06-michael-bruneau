0cc3f1cd520743acb876fed9b58d434de74325a9
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-09-29 12:44:15 -0400

    Setup

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael B
+ * Created on: Sep 2023
+ * This program turns LED on and off
 */
+
+// setup
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16,0)
+basic.showIcon(IconNames.Happy)
 
 basic.showString('Hello, World!')
