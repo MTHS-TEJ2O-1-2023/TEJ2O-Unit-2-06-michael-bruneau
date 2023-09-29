520e14953e61c20dda846b6f364d2cc2e3bae341
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-09-29 20:23:22 +0000

    Python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,23 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Michael B
+Created on: Sep 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
+# setup
+display.clear()
+pin16.write_digital(0)
+display.show(Image.Happy)
 
-display.scroll("Hello, World!")
+# turn on LED
+while True:
+    if button_a.is_pressed():
+        pin16.write_digital(1)
+        display.show(Image.Yes)
+
+while True:
+    if button_a.is_pressed():
+        pin16.write_digital(0)
+        display.show(Image.No)
